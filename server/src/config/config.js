7d367ca04bb18bdbbec2b91e25550a9a955052ce
sachinkodkani <sachin.kodkani@gmail.com>
2018-12-31 11:43:14 +0530

    Added development and production configuration

---
+++ server/src/config/config.js
@@ -0,0 +1,15 @@
+var configuration = {
+  development: {
+    db: "mongodb://127.0.0.1/test",
+    app: {
+      name: "graphql"
+    }
+  },
+
+  production: {
+    db: "mlabLink",
+    app: {
+      name: "graphql"
+    }
+  }
+};
