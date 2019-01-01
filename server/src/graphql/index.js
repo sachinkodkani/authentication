dee49849b88ad52cbf70ebd0151ed1593bdc5644
sachinkodkani <sachin.kodkani@gmail.com>
2019-01-01 15:35:13 +0530

    updated the file for modularization of schemas and resolvers

--- server/src/graphql/index.js
+++ server/src/graphql/index.js
@@ -1,19 +1,31 @@
 import { makeExecutableSchema } from "graphql-tools";
 import { merge } from "lodash";
 
-import { typeDef as User, resolvers as userResolver } from "./queries/user";
+import { 
+  typeDef as User, 
+  resolvers as userResolver 
+} from "./queries/user";
 
-const Query = `
-  type Query {
-    _empty: String
+import {
+  typeDef as CreateUser,
+  resolvers as createUserResolver
+} from "./mutations/user";
+
+const RootTypeDefs = `
+  
+  type Query 
+  type Mutation
+
+  schema {
+    query : Query
+    mutation : Mutation
   }
+
 `;
 
-const resolvers = {};
-
 var schema = makeExecutableSchema({
-  typeDefs: [ Query, User ],
-  resolvers: merge(resolvers, userResolver)
+  typeDefs: [RootTypeDefs, User, CreateUser],
+  resolvers: merge(userResolver, createUserResolver)
 });
 
 export { schema };
