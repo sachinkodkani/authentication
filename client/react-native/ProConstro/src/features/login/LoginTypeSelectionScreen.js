8a9feebffa93cbd518cbc09b511c325e397312ae
sachinkodkani <sachin.kodkani@gmail.com>
2019-02-17 00:43:40 +0530

    First UI adaption changes

--- client/react-native/ProConstro/src/features/login/LoginTypeSelectionScreen.js
+++ client/react-native/ProConstro/src/features/login/LoginTypeSelectionScreen.js
@@ -4,13 +4,29 @@ import {
   heightPercentageToDP
 } from "react-native-responsive-screen";
 import styled from "styled-components/native";
-import images from 'res/images';
+import images from "res/images";
 
 export default class LoginTypeSelectionScreen extends React.Component {
   render() {
     return (
       <Container>
-
+        <Header>
+          <HeaderText>Choose Your Role!</HeaderText>
+        </Header>
+        <Body>
+          <Segment>
+            <UserSelectionButton>
+              <UserIcon source={{ uri: images.adminIcon }} />
+            </UserSelectionButton>
+            <UserSelectionButton>
+              <UserIcon source={{ uri: images.employeeIcon }} />
+            </UserSelectionButton>
+            <UserSelectionButton>
+              <UserIcon source={{ uri: images.clientIcon }} />
+            </UserSelectionButton>
+          </Segment>
+        </Body>
+        <Footer />
       </Container>
     );
   }
@@ -35,16 +51,24 @@ const HeaderText = styled.Text`
 
 const Body = styled.View`
   flex-direction: column;
-  background-color : ${props.theme.background.PRIMARY_BACKGROUND_COLOR};
+  background-color: ${props.theme.background.PRIMARY_BACKGROUND_COLOR};
 `;
 
 const Segment = styled.View`
   flex-direction: column;
 `;
 
+const UserSelectionButton = styled.TouchableOpacity`
+  flex-direction: row;
+  opacity: 0;
+`;
+
+const UserIcon = styled.Image`
+  height: 30;
+  width: 30;
+`;
+
 const Footer = styled.View`
   flex-direction: row;
   background-color: ${props => props.theme.background.PRIMARY_BACKGROUND_COLOR};
 `;
-
-
