fdbe4796e4960e6388bc571d68576b5d74e14eec
sachinkodkani <sachin.kodkani@gmail.com>
2019-02-17 19:02:57 +0530

    Moved client to client development repo

--- client/react-native/ProConstro/src/features/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import themeReducer from './theme/themeReducer';
-
-export default combineReducers({
-    themeReducer
-});
--- client/react-native/ProConstro/src/res/images.js
+++
@@ -1,9 +0,0 @@
-//not working in imports
-
-const images = {
-    adminIcon: require("./images/admin.png"),
-    employeeIcon: require("./images/employee.png"),
-    clientIcon: require("./images/client.png")
-}
-
-export default images;
